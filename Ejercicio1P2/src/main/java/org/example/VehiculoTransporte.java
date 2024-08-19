f8cb1bc897655a50164192886a2dc0b84eed14b0
LeidyVane <leidyv.munozb@uqvirtual.edu.co>
2024-08-19 15:01:00 -0500

    corregí errores de sintaxis

--- Ejercicio1P2/src/main/java/org/example/VehiculoTransporte.java
+++ Ejercicio1P2/src/main/java/org/example/VehiculoTransporte.java
@@ -4,8 +4,8 @@ package org.example;
 public class VehiculoTransporte extends Vehiculo {
     private int maxPasajeros;
 
-    public VehiculoTransporte(String placa, String modelo, String marca, String color, int maxPasajeros) {
-        super(placa, modelo, marca, color);
+    public VehiculoTransporte(String placa, String modelo, String marca, String color, int maxPasajeros, double peso) {
+        super(placa, modelo, marca, color, peso);
         this.maxPasajeros = maxPasajeros;
     }
     
